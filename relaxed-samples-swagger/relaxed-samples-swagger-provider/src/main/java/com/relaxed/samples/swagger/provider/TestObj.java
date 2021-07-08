4fb944e6f585957f6261524c50a1b81577340932
Yakir <1412844668@qq.com>
2021-07-08 17:04:52 +0800

    :art:  swagger validator

--- relaxed-samples-swagger/relaxed-samples-swagger-provider/src/main/java/com/relaxed/samples/swagger/provider/TestObj.java
+++ relaxed-samples-swagger/relaxed-samples-swagger-provider/src/main/java/com/relaxed/samples/swagger/provider/TestObj.java
@@ -5,6 +5,10 @@ import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author Yakir
@@ -21,11 +25,13 @@ public class TestObj {
      * 用户名
      */
     @ApiModelProperty("用户名")
+    @NotNull
     private String userName;
     /**
      * 年龄
      */
-    @ApiModelProperty("年龄")
+    @ApiModelProperty(value = "年龄",required = true)
+    @Min(value = 2)
     private Integer age;
 
     private Boolean open;
