dea7cfa75d1579d2f785dbcfc3cd726368c48e51
1412844668@qq.com <12345698aA+>
2022-06-16 11:20:48 +0800

    feature(doc):  扩展open api

--- relaxed-samples-swagger/relaxed-samples-springdoc-provider/src/main/java/com/relaxed/samples/doc/provider/TestObj.java
+++ relaxed-samples-swagger/relaxed-samples-springdoc-provider/src/main/java/com/relaxed/samples/doc/provider/TestObj.java
@@ -1,5 +1,6 @@
 package com.relaxed.samples.doc.provider;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +9,17 @@ import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema
 @Data
 public class TestObj {
 
+	@Schema
 	private Integer userId;
 
+	@Schema(title = "姓名")
 	private String name;
 
+	@Schema
 	private BigDecimal money;
 
 }
